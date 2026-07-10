beb2bb08f2b67a6decad3a571ce3ea15cf3e71ec
CveleFIT <cvetkojovicic995@gmail.com>
2026-07-10 08:07:00 +0200

    1.

--- crud.py
+++ crud.py
@@ -1,11 +1,36 @@
 from models import Gradiliste,Karnet,Zaposleni
 from database import session
 
-def novo_gradiliste(naziv, lokacija, investitor):
+def dodaj_gradiliste(naziv, lokacija, investitor):
     gradiliste = Gradiliste(naziv=naziv, lokacija = lokacija, investitor = investitor)
     session.add(gradiliste)
     session.commit()
     print("Gradiliste je dodato.")
+
+def prikazi_sva_gradilista():
+    return session.query(Gradiliste).all()
+
+def pronadji_gradiliste(id_gradilista):
+    return session.query(Gradiliste).filter_by(id = id_gradilista).first()
+    
+
+def promeni_gradiliste(id_gradilista, novi_naziv, nova_lokacija, nov_investitor):
+    izabrano_gradiliste = pronadji_gradiliste(id_gradilista)
+    if not izabrano_gradiliste:
+        print("Gradiliste ne postoji.")
+        return
+    izabrano_gradiliste.naziv = novi_naziv
+    izabrano_gradiliste.lokacija = nova_lokacija
+    izabrano_gradiliste.investitor = nov_investitor
+    session.commit()
+
+def obrisi_gradiliste(id_gradilista):
+    trazeno_gradiliste = pronadji_gradiliste(id_gradilista)
+    if not trazeno_gradiliste:
+        print("Gradiliste ne postoji.")
+        return
+    session.delete(trazeno_gradiliste)
+    session.commit()
 
 def novi_zaposleni(ime, radno_mesto, satnica):
     novi_radnik = Zaposleni(ime_prezime = ime, radno_mesto = radno_mesto, satnica=satnica)
